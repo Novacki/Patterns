6ff622329df28e7cc6570981e68c64add7adc589
FERNANDO NOVACKI FREITAS <fernando.freitas3@fatec.sp.gov.br>
2021-12-24 15:18:59 -0300

    adding pattern Chain of Responsibility

---
+++ DesignPatterns/Behavioral/ChainOfResponsibility/Middlewares/CheckPasswordMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility.Middlewares
+{
+    class CheckPasswordMiddleware : Middleware
+    {
+        public override bool Check(string email, string password)
+        {
+            if (password.Equals("123456"))
+                Console.WriteLine("Sugerimos que altere o seu password.");
+
+
+            return CheckNext(email, password);
+        }
+    }
+}
